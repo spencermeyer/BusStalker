6ade8bf1fe1a8e8d702ef5223460fc8bea5cf1d9
spencermeyer <spencer_meyer@hotmail.com>
2015-08-13 10:58:19 +0100

    now lists all the stops for a bus

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 API_Example_TFL  THIS IS WORK IN PROGRESS: 12 AUG 2015
 
-This application pulls TFL Live Data and Displays.  Uses aspects of AJAX, Angular, Vanilla JavaScript, and Bootstrap.
+This application pulls TFL Live Data and Displays.  Uses aspects of AJAX, Angular, Vanilla JavaScript, JQuery, and Bootstrap.
 
 Live data is arriving - see console log!
 Now to do something useful with it....
--- busController.js
+++ busController.js
@@ -1,14 +1,25 @@
+//  API Demo by Spencer M.
 
+//  This section gets the data from TFL's JSON feed:
 
-var app = angular.module('myApp', []);
-app.controller('busCtrl', function($scope, $http) {
-  $http.get("https://api.tfl.gov.uk/line/25/arrivals")
-  .success(function (response) {$scope.names = response.records;
-    console.log("and this is the success function callback");
-    document.getElementById("station").innerHTML=response[1].stationName;
+  var app = angular.module('myApp', []);
+  app.controller('busCtrl', function($scope, $http) {
+    $http.get("https://api.tfl.gov.uk/line/25/arrivals")
+    .success(function (response) {$scope.names = response.records;
+      console.log("and this is the success function callback");
+
+      for (i=0; i<response.length; i++) {
+        $("#station").append("<p>"+response[i].stationName+"</p>");
+      }
+
+      // document.getElementById("station").innerHTML=response[0].stationName;
+
+      
+      // $("#station").append("<p>"+"and there are "+response.length+" objects"+"</p>");
+
     // console.log(response[1].stationName)});
     console.log(response)});
-});
+  });
 
 
 
