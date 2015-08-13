191111e47583b845e99b49dd4fd05f5f4bbcf857
spencermeyer <spencer_meyer@hotmail.com>
2015-08-13 22:52:19 +0100

    got four columns of data showing in AJAX view

--- busController.js
+++ busController.js
@@ -22,9 +22,8 @@ app.controller('busCtrl', function($scope, $http) {
     console.log("and this is the success function callback");
 
     for (i=0; i<response.length; i++) {
-      $("#station").append('<div class="row"><div class="col-sm-3"><div class="form-group"><p>'+response[i].stationName+'</p></div></div><div class="col-sm-4"><div class="form-group"><p>'+response[i].vehicleId+'</p></div></div></div>');
+      $("#station").append('<div class="row"><div class="col-sm-3"><div class="form-group"><p>'+response[i].stationName+'</p></div></div><div class="col-sm-2"><div class="form-group"><p>'+response[i].vehicleId+'</p></div></div><div class="col-sm-4"><div class="form-group"><p>'+response[i].towards+'</p></div></div><div class="col-sm-2"><div class="form-group"><p>'+response[i].timeToStation+'</p></div></div>     </div>');
     }
-
 
   console.log(response)});
 });
