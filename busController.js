0969929321f22e07f1cc6f50b7f6290a5e4c81f6
spencermeyer <spencer_meyer@hotmail.com>
2015-08-13 17:12:35 +0100

    got bus input box working and logging selected bus

--- busController.js
+++ busController.js
@@ -1,16 +1,29 @@
 //  API Demo by Spencer M.
 
-//  This section gets the data from TFL's JSON feed:
+$( document ).ready(function() {
+// First to pick up the desired bus from the user input
+  busSelected = document.getElementById("usr").value;
+  console.log("and the selected bus is " + busSelected);
+  console.log( "ready!" );
+  $( "#clickBus1" ).click(function() {
+  console.log("Handler for busSelected.click() called." );
+  });
+});
 
-  var app = angular.module('myApp', []);
-  app.controller('busCtrl', function($scope, $http) {
-    $http.get("https://api.tfl.gov.uk/line/25/arrivals")
-    .success(function (response) {$scope.names = response.records;
-      console.log("and this is the success function callback");
+// check the on click action
 
-      for (i=0; i<response.length; i++) {
-        $("#station").append("<p>"+response[i].stationName+"</p>");
-      }
+
+//  This section gets the data from TFL's JSON feed
+
+var app = angular.module('myApp', []);
+app.controller('busCtrl', function($scope, $http) {
+  $http.get("https://api.tfl.gov.uk/line/25/arrivals")
+  .success(function (response) {$scope.names = response.records;
+    console.log("and this is the success function callback");
+
+    for (i=0; i<response.length; i++) {
+      $("#station").append("<p>"+response[i].stationName+"</p>");
+    }
 
       // document.getElementById("station").innerHTML=response[0].stationName;
 
@@ -18,8 +31,8 @@
       // $("#station").append("<p>"+"and there are "+response.length+" objects"+"</p>");
 
     // console.log(response[1].stationName)});
-    console.log(response)});
-  });
+  console.log(response)});
+});
 
 
 
